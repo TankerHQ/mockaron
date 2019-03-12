cd68bdbc071d64bf426191ade953d6277a7b044c
Théo DELRIEU <theo.delrieu@tanker.io>
2019-03-12 16:04:17 +0100

    use latest CI scripts

--- run-ci.py
+++ run-ci.py
@@ -14,6 +14,7 @@ import ci.git
 
 def main() -> None:
     parser = argparse.ArgumentParser()
+    parser.add_argument("--isolate-conan-user-home", action="store_true", dest="home_isolation", default=False)
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
 
     build_and_test_parser = subparsers.add_parser("build-and-test")
@@ -23,9 +24,11 @@ def main() -> None:
     subparsers.add_parser("deploy")
     subparsers.add_parser("mirror")
 
-    ci.cpp.update_conan_config()
-
     args = parser.parse_args()
+    if args.home_isolation:
+        ci.cpp.set_home_isolation()
+
+    ci.cpp.update_conan_config()
     if args.command == "build-and-test":
         ci.cpp.check(args.profile, coverage=args.coverage, run_tests=True)
     elif args.command == "deploy":
