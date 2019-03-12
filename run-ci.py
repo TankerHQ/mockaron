1f3ebd4b701b2cb39350ae469ce466e67795fd63
Théo DELRIEU <theo.delrieu@tanker.io>
2019-03-12 15:53:48 +0100

    revert all broken commits

--- run-ci.py
+++ run-ci.py
@@ -14,7 +14,6 @@ import ci.git
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--isolate-conan-user-home", action="store_true", dest="home_isolation", default=False)
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
 
     build_and_test_parser = subparsers.add_parser("build-and-test")
@@ -24,24 +23,18 @@ def main() -> None:
     subparsers.add_parser("deploy")
     subparsers.add_parser("mirror")
 
-    args = parser.parse_args()
-    if args.home_isolation:
-        ci.cpp.set_home_isolation()
-
     ci.cpp.update_conan_config()
 
+    args = parser.parse_args()
     if args.command == "build-and-test":
         ci.cpp.check(args.profile, coverage=args.coverage, run_tests=True)
     elif args.command == "deploy":
-        git_tag = os.environ.get("CI_COMMIT_TAG")
-        conan_reference = None
-        if git_tag:
-            version = ci.version_from_git_tag(git_tag)
-            ci.bump_files(version)
-            conan_reference=f"tanker/{version}@tanker/stable"
+        git_tag = os.environ["CI_COMMIT_TAG"]
+        version = ci.version_from_git_tag(git_tag)
+        ci.bump_files(version)
         ci.cpp.build_recipe(
             Path.getcwd(),
-            conan_reference=conan_reference,
+            conan_reference=f"tanker/{version}@tanker/stable",
             upload=True,
         )
     elif args.command == "mirror":
